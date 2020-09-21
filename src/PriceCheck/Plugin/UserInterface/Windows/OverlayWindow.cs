9a84bc0dc79d1750e91eb33e8e0f632c072dae4b
kalilistic <35899782+kalilistic@users.noreply.github.com>
2020-09-21 18:39:23 -0400

    adjust overlay size

--- src/PriceCheck/Plugin/UserInterface/Windows/OverlayWindow.cs
+++ src/PriceCheck/Plugin/UserInterface/Windows/OverlayWindow.cs
@@ -8,6 +8,7 @@ namespace PriceCheck
 	{
 		private readonly IPluginWrapper _plugin;
 		private readonly IPriceService _priceService;
+		private float _uiScale;
 
 		public OverlayWindow(IPluginWrapper plugin, IPriceService priceService)
 		{
@@ -18,8 +19,8 @@ namespace PriceCheck
 		public void DrawWindow()
 		{
 			if (!IsVisible) return;
-
-			ImGui.SetNextWindowSize(new Vector2(375, 330), ImGuiCond.FirstUseEver);
+			_uiScale = ImGui.GetIO().FontGlobalScale;
+			ImGui.SetNextWindowSize(new Vector2(300 * _uiScale, 150 * _uiScale), ImGuiCond.FirstUseEver);
 			if (ImGui.Begin(Loc.Localize("OverlayWindow", "PriceCheck Overlay") + "###PriceCheck_Overlay_Window",
 				ref IsVisible,
 				ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
